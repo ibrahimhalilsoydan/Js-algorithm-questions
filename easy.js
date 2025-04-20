23666795b66966fe074ce52e07b0159f8e5f5390
ibrahim halil soydan <ibrahimhalilsoydan@gmail.com>
2025-04-20 10:57:26 +0300

    firstFactorial added

--- easy.js
+++ easy.js
@@ -2,5 +2,7 @@ import { userValidation } from "./easy-questions/1_userValidation.js";
 //console.log(userValidation("u__hello_world123"));
 
 import { longestWord } from "./easy-questions/2_longestWord.js";
-console.log(longestWord("fun&!! time"));
+//console.log(longestWord("fun&!! time"));
 
+import { firstFactorial } from "./easy-questions/3_firstFactorial.js";
+console.log(firstFactorial(8));
